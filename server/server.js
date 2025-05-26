230ee780645c72e784c91c7c6aa0397a0398ffac
Masuma <masumaakterakhi90@gmail.com>
2025-05-26 23:35:09 +0600

    some update cod server file changes

--- server/server.js
+++ server/server.js
@@ -29,9 +29,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 app.use(session ({
