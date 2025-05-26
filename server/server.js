56f518b8677a9809290a8631695e98c9fbfca1ea
Masuma Akter Akhi <130857056+masumaakhi@users.noreply.github.com>
2025-05-27 02:07:16 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(
   cors({
     origin: "https://mern-auth-fronted.onrender.com",
     credentials: true,
-    methods: "GET", "POST", "PUT", "DELETE"
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 
