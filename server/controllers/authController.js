ba666ae301deed679bb27ddd883642fa08aff9f8
Masuma Akter Akhi <130857056+masumaakhi@users.noreply.github.com>
2025-05-28 16:00:48 +0600

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -111,8 +111,9 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            secure: true,
+            sameSite: "None",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         return res.json({ success: true, message: "Logout Successful" });
